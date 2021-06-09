fe843db05b3112fbbfdcdf4ee491863763abd20c
Lukas Andriuška <60364075+TheLuk1s@users.noreply.github.com>
2021-06-09 11:56:07 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Card name="vardas" link="https://www.google.com/" :tweets="3" />
+  </div>
+</template>
+
+<script>
+/*import HelloWorld from "./components/HelloWorld";*/
+import Card from "./components/Card";
+
+export default {
+  name: "App",
+  components: {
+    Card
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="card">
+    <!-- Image wrapper -->
+    <div class="card-image--wrapper">
+      <img class="card-image" src="https://picsum.photos/200">
+    </div>
+
+    <!-- Name wrapper -->
+    <div class="card-name--wrapper">
+      <a :href="link">{{ name }}</a>
+    </div>
+
+    <!-- Stats wrapper -->
+    <div class="card-numbers--wrapper">
+      <div class="row">
+        <div class="flex-item">Tweets</div>
+        <div class="flex-item">Following</div>
+        <div class="flex-item">Followers</div>
+      </div>
+      <div class="row">
+        <div class="flex-item">{{ tweets }}</div>
+        <div class="flex-item">{{ following }}</div>
+        <div class="flex-item">{{ followers }}</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    tweets: {
+        type: Number,
+        default: 0
+    },
+    following: {
+        type: Number,
+        default: 0
+    },
+    followers: {
+        type: Number,
+        default: 0
+    },
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .card {
+    width: 300px;
+    background-color: #595FFF;
+    display: block;
+    border: 1px solid #000;
+  }
+
+  .card-image--wrapper {
+    padding: 20px 20px 10px 20px;
+    img {
+      width: 100%;
+      border: 1px solid #000;
+    }
+  }
+  
+  .card-name--wrapper {
+      a {
+        text-transform: uppercase;
+        text-decoration: none;
+        color: #fff;
+      }
+  }
+
+  .card-numbers--wrapper {
+      margin-top: 15px;
+      margin-bottom: 20px; 
+  }
+
+  .row{
+    display: flex;
+    flex-flow: row wrap;
+    
+    .flex-item {
+      color: #000;
+      flex: 0 1 33%;
+    }
+  }
+</style>
